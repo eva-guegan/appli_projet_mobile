5baafc65706d07698ee36e76edf27b66f287abad
owen boutet <owenboutet@gmail.com>
2021-03-12 09:50:00 +0100

    module en double

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import VueSession from 'vue-session'
 
