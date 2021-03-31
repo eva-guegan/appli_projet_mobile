b9f92f49ce043cb2bfe07ce14536a7e93b23fc3c
eva-guegan <45195610+eva-guegan@users.noreply.github.com>
2021-03-31 16:12:08 +0200

    Revert "merged to master"

--- src/views/DetailUser.vue
+++ src/views/DetailUser.vue
@@ -8,7 +8,7 @@
       <p><b>Prenom :</b> {{ userDetails.prenom }}</p>
       <p><b>Mail :</b> {{ userDetails.email }}</p>
       <p><b>Date d'inscription :</b> {{ userDetails.dateInscription }}</p>
-      <p v-if="userDetails.isAdmin === 1"><b>Role :</b> Admin</p>
+      <p v-if="isAdmin === 1"><b>Role :</b> Admin</p>
       <p v-else><b>Role :</b> User</p>
     </div>
 
@@ -21,6 +21,7 @@ export default {
   data() {
     return {
       userDetails: {},
+      isAdmin: {}
     }
   },
   created() {
@@ -32,6 +33,8 @@ export default {
           .get(this.$root.baseUserApi + "users/" + this.$route.params.idUser)
           .then((response) => {
             this.userDetails = response.data
+            this.isAdmin = response.data.isAdmin.data[0]
+            console.log(this.userDetails)
           });
     }
   }
